a29babbfaebba86864fddeb8815626177438920d
Adriano Almeida <adrianoo.luis.almeida@gmail.com>
2021-12-27 09:40:06 -0300

    first commit

---
+++ src/routes.ts
@@ -0,0 +1,30 @@
+import { Router } from "express";
+import { AuthenticateClientController } from "./modules/account/authenticateClient/AuthenticateClientController";
+import { AuthenticateDeliverymanController } from "./modules/account/authenticateDeliveryman/AuthenticateDeliverymanController";
+import { CreateClientController } from "./modules/clients/useCases/createClient/CreateClientController";
+import { CreateDeliverymanController } from "./modules/deliveryman/useCases/createDeliveryman/CreateDeliverymanController";
+import { CreateDeliveryController } from "./modules/deliveries/useCases/createDelivery/CreateDeliveryController";
+import { FindAllWithoutEndDateController } from "./modules/deliveries/useCases/findAllWithoutEndDate/FindAllWithoutEndDateController";
+
+import { ensureAuthenticateClient } from "./middlewares/ensureAuthenticateClient";
+import { ensureAuthenticateDeliveryman } from "./middlewares/ensureAuthenticateDeliveryman";
+
+const routes = Router();
+
+const createClientController = new CreateClientController();
+const authenticateClientController = new AuthenticateClientController();
+const createDeliverymanController = new CreateDeliverymanController();
+const authenticateDeliverymanController = new AuthenticateDeliverymanController();
+const createDeliveryController = new CreateDeliveryController();
+const findAllWithoutEndDateController = new FindAllWithoutEndDateController();
+
+routes.post("/client/authenticate", authenticateClientController.handle);
+routes.post("/deliveryman/authenticate", authenticateDeliverymanController.handle);
+
+routes.post("/client", createClientController.handle);
+routes.post("/deliveryman", createDeliverymanController.handle);
+routes.post("/delivery", ensureAuthenticateClient, createDeliveryController.handle);
+routes.get("/delivery/available", ensureAuthenticateDeliveryman, findAllWithoutEndDateController.handle);
+
+export { routes };
+
